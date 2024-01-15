e104db677003d3c507305f9e2a582818baded222
usmonovdev <usmonovazizbek045@gmail.com>
2024-01-15 20:23:59 +0500

    card details

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }:CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
@@ -10,11 +18,19 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType }:CustomBut
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>
-        {title}
-      </span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
--- components/index.ts
+++ components/index.ts
@@ -5,5 +5,17 @@ import Footer from "./Footer";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
-export { Hero, CustomButton, Footer, Navbar, CustomFilter, SearchBar, SearchManufacturer };
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+};
