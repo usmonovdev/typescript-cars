cb66f22fa4515d224d5e29dea248beda2fb68e98
usmonovdev <usmonovazizbek045@gmail.com>
2024-01-15 17:03:01 +0500

    search manufacturer added

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,8 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export {
-  Hero,
-  CustomButton,
-  Footer,
-  Navbar
-}
+export { Hero, CustomButton, Footer, Navbar, CustomFilter, SearchBar, SearchManufacturer };
